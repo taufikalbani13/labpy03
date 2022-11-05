c9b195cd89e2dda795c2d97903d1fe97e80e7ca3
taufik eka albani <taufikalbani144@gmail.com~>
2022-11-05 22:40:56 +0700

    Menambahkan folder

---
+++ Praktikum2/Praktikum2.py
@@ -0,0 +1,9 @@
+a=int(input("masukan bilangan ke-1: "))
+b=int(input("masukan bilangan ke-2: "))
+c=int(input("masukan bilangan ke-3: "))
+if (a>b and a>c):
+    print("hasil bilangan tebesar adalah : ",a)
+elif (b>a and b>c):
+    print("hasil bilangan terbesar adalah : ",b)
+else :
+    print("hasil bilangan terbesar adalah : ",c)
---
+++ Praktikum3/program1.py
@@ -0,0 +1,17 @@
+x=int(input("modal awal : "))
+
+a=0*x
+b=0*x
+c=0.1*x
+d=0.1*x
+e=0.5*x
+f=0.5*x
+g=0.5*x
+h=0.3*x
+t=[a,b,c,d,e,f,g,h]
+
+for i in range (len(t)) :
+    print("laba bulan ke-",i+1,"sebesar : ",t[i])
+
+u= (a+b+c+d+e+f+g+h)
+print("total laba adalah : ",u)
